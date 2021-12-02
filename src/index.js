f3b94a5ec1dace94992e23d65541ca79f7060b6d
forysiak-vova <volodja.forus@gmail.com>
2021-12-02 20:36:20 +0100

    e

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './sass/main.scss';
+
+
+const form = document.getElementById('search-form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+   e.preventDefault();
+   const getValue = e.currentTarget.elements.searchQuery.value;
+   console.log(getValue);
+}
+
+// async function getFruits(name) {
+//    const fruit = {
+//       strawberry: '1',
+//       kiwi: '2',
+//       cherry:'3',
+//    }
+//    return Promise.resolve(fruit[name])
+// }
+//  getFruits().then(console.log)
